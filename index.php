cdf31ea0da7c35f2c37a94f08518739ce134a2b7
st-0403-jp <sato252011@gmail.com>
2017-05-19 21:05:28 +0900

    【add】PHP準備

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$a=1;
+$b=2;
+$c=$a+$b;
+echo $c;
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>kidsQ｜クイズアプリ</title>
+    </head>
+    <body>
+        <p>テスト</p>
+    </body>
+</html>
