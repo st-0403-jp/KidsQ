9730f2c51a173e2c2b2743014af0e370ed07af51
st-0403-jp <sato252011@gmail.com>
2017-05-20 23:47:14 +0900

    【add】bower入れる、sanitize.css追加、ejs追加、layout.ejsで共通化

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,31 @@ var gulp = require("gulp");
 var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var less = require('gulp-less');
+var ejs = require('gulp-ejs');
 
-gulp.task('php', function () {
-    return gulp.src('src/php/index.php')
+var ejsData = {
+    data: 'ejsejsejs'
+};
+
+gulp.task('ejs', function () {
+    return gulp.src(['src/ejs/layout.ejs'], {base: 'src/ejs'})
+            .pipe(ejs(ejsData, null, {ext: '.php'}))
             .pipe(gulp.dest('build'));
 });
 gulp.task('less', function () {
     return gulp.src('src/less/common.less')
             .pipe(less())
             .pipe(gulp.dest('build/css'));
+});
+gulp.task('watch', function () {
+    gulp.watch(['src/ejs/*.ejs'], ['ejs']);
+    gulp.watch(['src/less/*.less'], ['less']);
+});
+gulp.task('clean', function () {
+    gulp.src('build/*').pipe(clean());
+});
+gulp.task('lib', function () {
+   gulp.src('src/lib/*').pipe(gulp.dest('build/lib')); 
 });
 
 gulp.task('default', function () {
